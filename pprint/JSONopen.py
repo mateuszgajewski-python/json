1e9f386c4469a99576d77da16596f0d6b6a08461
Mateusz Gajewski <mateuszgajewski@gmail.com>
2021-02-08 18:50:13 +0100

    21.02.08

---
+++ pprint/JSONopen.py
@@ -0,0 +1,15 @@
+import json
+import pprint
+
+encodedMovie = ('{"title": "Ale ja nie bede tego robil!", "release_year": 1969, "won_oscar": true, "actors": ["Arkadiusz Wlodarczyk", "Wiolleta Wlodarczyk"], "budget": null, "credits": {"director": "Arkadiusz Wlodarczyk", "writer": "Alan Burger", "animator": "Anime Animatrix"}}')
+
+
+decodedMovie = json.loads(encodedMovie)
+
+with open("sample.json", encoding = "UTF-8") as file:
+    wynik = json.load(file)
+
+
+print(wynik)
+
+pprint.pprint(wynik)
---
+++ pprint/JSONsave.py
@@ -0,0 +1,24 @@
+import json
+
+film =  {
+        "title" : "Ale ja nie bede tego robil!",
+        "release_year" : 1969,
+        "won_oscar" : True,
+        "actors": ("Arkadiusz Wlodarczyk", "Wiolleta Wlodarczyk"),
+        "budget" : None,
+        "credits" : {
+                    "director" : "Arkadiusz Wlodarczyk",
+                    "writer" : "Alan Burger",
+                    "animator" : "Anime Animatrix"
+                    }
+        }
+
+encodedFilm = json.dumps(film, ensure_ascii = False, indent = 4, sort_keys = True)
+
+with open("sample.json", "w", encoding = "UTF-8") as file:
+    json.dump(film, file, ensure_ascii = False, indent = 4, sort_keys = True)
+
+
+print(encodedFilm)
+
+
